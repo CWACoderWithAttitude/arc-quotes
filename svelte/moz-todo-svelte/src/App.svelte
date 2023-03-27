4af95cc1c280145f129352eaf2084751be73cc85
Volker Benders <vbenders@gmail.com>
2023-03-27 19:32:12 +0000

    removed commented stuff, reformat

--- svelte/moz-todo-svelte/src/App.svelte
+++ svelte/moz-todo-svelte/src/App.svelte
@@ -1,45 +1,19 @@
 <script>
   import { onMount } from "svelte";
 
+  export let name;
+  export let quotes;
+  // https://medium.com/@TimvanBaarsen/how-to-connect-to-the-docker-host-from-inside-a-docker-container-112b4c71bc66
+  const quotesApi = "/api/quotes";
 
-	export let name;
-	export let quotes;
-	// https://medium.com/@TimvanBaarsen/how-to-connect-to-the-docker-host-from-inside-a-docker-container-112b4c71bc66
-	const quotesApi = "http://host.docker.internal:3333/api/quotes"
-	//const quotesApi = "http://192.168.65.2:3333/api/quotes"
-	//const quotesApi = "http://localhost:3333/api/quotes"
-	//const quotesApi = "http://localhost:3333/api/quotes"
-	//const quotesApi = "/api/quotes"
+  onMount(async () => {
+    console.log("onMount: begin...");
+    const response = await fetch(quotesApi);
+    quotes = await response.json();
 
-	onMount(async () => {
-		console.log("onMount: begin...")
-		/* fetch(quotesApi)
-  			.then(response => response.json())
-			.then(data => {
-					console.log(">>>>>>>"+data);
-				data = data;
-			}).catch(error => {
-				console.log(error);
-				//return [{"quoteID":"1", "text": "bla fasel blubb", "author": "erwin"}];
-				data = [{"quoteID":"1", "text": "bla fasel blubb", "author": "erwin"}];
-			}); */
-		//response = await fetch('http://127.0.0.1:3333/api/quotes');
-		const response = await fetch('/api/quotes');
-		//const response = await fetch(quotesApi)
-		//data = await fetch(quotesApi).then((x)=>x.json())
-		quotes = await response.json()
-
-	//.then(response => response.json())
-	//.then(response => console.log(JSON.stringify(response, ' ', 2)))
-		
-		//console.log("onMount: response. " + JSON.stringify(response))
-		//console.log("onMount: json: "+json != undefined ? JSON.stringify(json) : 'n/a')
-		//console.log("onMount: response: "+response != undefined ? JSON.stringify(response) : 'n/a')
-
-// svelte recipes https://github.com/svelte-society/recipes-mvp/blob/master/components.md#using-fetch-to-consume-apis-with-svelte
-console.log("onMount: end")
-	});
-	function toggleName() {
+    console.log("onMount: end");
+  });
+  function toggleName() {
     if (name === "world") {
       name = "Svelte";
     } else {
@@ -49,53 +23,54 @@ console.log("onMount: end")
 </script>
 
 <main>
-	<h1>Hello {name} from Outer Space!</h1>
-<pre>
+  <h1>Hello {name} from Outer Space!</h1>
+  <pre>
 	
 abc
 {quotes}
 zzz
 	{#await quotes}
-	aaa
-		
-	{/await}
+      aaa
+    {/await}
 
 	{#if quotes}
-		Daten
+      Daten
 		{#each quotes as x}
-			<div>
+        <div>
 				<div>{x.author}</div>
 				<div>{x.text}</div>
 			</div>
-		{/each}
-	{:else}
-	  Bitte warte, ich lese Daten...
-	{/if}
+      {/each}
+    {:else}
+      Bitte warte, ich lese Daten...
+    {/if}
 </pre>
 
-	<button on:click="{toggleName}">Toggle name</button>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	
+  <button on:click={toggleName}>Toggle name</button>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
