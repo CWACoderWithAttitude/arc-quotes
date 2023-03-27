f73964902485347819e8429f6caf082668e94508
Volker Benders <vbenders@gmail.com>
2023-03-27 19:37:51 +0000

    polishing

--- svelte/moz-todo-svelte/src/main.js
+++ svelte/moz-todo-svelte/src/main.js
@@ -1,11 +1,10 @@
 import App from './App.svelte';
 
-const quotes = [{"author":"aldnlnk", "text":"fö,väöf"},{"author":"aldnlnk", "text":"fö,väöf"},{"author":"aldnlnk", "text":"fö,väöf"}]
 const app = new App({
 	target: document.body,
 	props: {
 		name: 'stranger',
-		quotes: quotes
+		quotes: undefined
 	}
 });
 
