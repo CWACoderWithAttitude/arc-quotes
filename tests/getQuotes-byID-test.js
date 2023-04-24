fe939e21ff3c529566fd8cd89874ff3700559c54
Volker Benders <vbenders@gmail.com>
2023-04-24 10:06:28 +0000

    wip

--- tests/getQuotes-byID-test.js
+++ tests/getQuotes-byID-test.js
@@ -1,6 +1,6 @@
-const test = require("tape");
-const tiny = require("tiny-json-http");
-const sandbox = require("@architect/sandbox");
+const test = require('tape');
+const tiny = require('tiny-json-http');
+const sandbox = require('@architect/sandbox');
 
 //
 // https://arc.codes/docs/en/guides/developer-experience/local-development
@@ -9,62 +9,58 @@ const sandbox = require("@architect/sandbox");
 /**
  * first we need to start the local http server
  */
-test("sandbox.start", async (t) => {
+test('sandbox.start', async (t) => {
   t.plan(1);
   await sandbox.start();
-  t.ok(true, "sandbox started and mockdata loaded on http://localhost:3333");
+  t.ok(true, 'sandbox started and mockdata loaded on http://localhost:3333');
 });
 
-test("endpoint exists", async (t) => {
+test('endpoint exists', async (t) => {
   t.plan(2);
-  let result = await tiny.get({ url: "http://localhost:3333/api/quotes/1" });
-  t.ok(result, "got result response");
-  t.notEqual(result.body, {}, "wrong bodfy?!");
+  let result = await tiny.get({ url: 'http://localhost:3333/api/quotes/1' });
+  t.ok(result, 'got result response');
+  t.notEqual(result.body, {}, 'wrong bodfy?!');
 });
-test("check valid result", async (t) => {
+test('check valid result', async (t) => {
   t.plan(2);
-  const id = "1";
+  const id = '1';
   let result = await tiny.get({
-    url: "http://localhost:3333/api/quotes/" + id,
+    url: 'http://localhost:3333/api/quotes/' + id,
   });
 
-  t.ok(result.body, "", "wew should have found something?!");
-  t.equal(result.body.quoteID, id, "ID mismatch - returned wrong quote by id?!");
+  t.ok(result.body, '', 'wew should have found something?!');
+  t.equal(result.body.quoteID, id, 'ID mismatch - returned wrong quote by id?!');
 });
 
-test("check query for non existing id", async (t) => {
+test('check query for non existing id', async (t) => {
   t.plan(1);
-  const nonExistingId = "99999";
+  const nonExistingId = '99999';
   let result = await tiny.get({
-    url: "http://localhost:3333/api/quotes/" + nonExistingId,
+    url: 'http://localhost:3333/api/quotes/' + nonExistingId,
   });
 
-  t.equal(result.body, "", "the JOSN should be empty?!");
+  t.equal(result.body, '', 'the JOSN should be empty?!');
 });
 
 test('get by existing id', async (t) => {
-  t.plan(2)
-  const existingId="1"
+  t.plan(2);
+  const existingId = '1';
   let result = await tiny.get({
-    url: "http://localhost:3333/api/quotes/" + existingId,
+    url: 'http://localhost:3333/api/quotes/' + existingId,
   });
-  t.ok(result, "there ain't a result?!")
-  j = JSON.stringify(result)
-  t.equal(result.body.quoteID, existingId, "the JOSN should be empty?!");
-})
-test("contenttype", async (t) => {
-  t.plan(3);
-  let result = await tiny.get({ url: "http://localhost:3333/api/quotes" });
-  t.ok(result.headers, "no headers at all?!");
-  t.ok(result.headers["content-type"], "no contewnrt tyoe header?!");
-  t.equal(
-    result.headers["content-type"],
-    "application/json; charset=utf8",
-    "wrong content type"
-  );
+  t.ok(result, "there ain't a result?!");
+
+  t.equal(result.body.quoteID, existingId, 'the JOSN should be empty?!');
 });
-test("teardown", async (t) => {
+test('contenttype', async (t) => {
+  t.plan(3);
+  let result = await tiny.get({ url: 'http://localhost:3333/api/quotes' });
+  t.ok(result.headers, 'no headers at all?!');
+  t.ok(result.headers['content-type'], 'no contewnrt tyoe header?!');
+  t.equal(result.headers['content-type'], 'application/json; charset=utf8', 'wrong content type');
+});
+test('teardown', async (t) => {
   t.plan(1);
   await sandbox.end();
-  t.ok(true, "sandbox ended");
+  t.ok(true, 'sandbox ended');
 });
--- tests/randomQuoteID-test.js
+++ tests/randomQuoteID-test.js
@@ -1,6 +1,6 @@
-const test = require("tape");
-const tiny = require("tiny-json-http");
-const sandbox = require("@architect/sandbox");
+const test = require('tape');
+const tiny = require('tiny-json-http');
+const sandbox = require('@architect/sandbox');
 //
 // https://arc.codes/docs/en/guides/developer-experience/local-development
 //
@@ -8,10 +8,10 @@ const sandbox = require("@architect/sandbox");
 /**
  * first we need to start the local http server
  */
-test("sandbox.start", async (t) => {
+test('sandbox.start', async (t) => {
   t.plan(1);
   await sandbox.start();
-  t.ok(true, "sandbox started and mockdata loaded on http://localhost:3333");
+  t.ok(true, 'sandbox started and mockdata loaded on http://localhost:3333');
 });
 
 // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
@@ -20,20 +20,20 @@ const generateId = async function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 const getRandomQuote = async function () {
-  const quotes = await tiny.get({ url: "http://localhost:3333/api/quotes" });
+  const quotes = await tiny.get({ url: 'http://localhost:3333/api/quotes' });
   const randomId = await generateId(0, quotes.length);
-  console.log("generateId > id: " + randomId);
+  console.log('generateId > id: ' + randomId);
   return quotes[randomId];
 };
-test("get random quote", async (t) => {
+test('get random quote', async (t) => {
   t.plan(1);
 
-  const quotes = await tiny.get({ url: "http://localhost:3333/api/randomQuote" });
-  t.true(quotes.body, "Randowm Quote should be valid");
+  const quotes = await tiny.get({ url: 'http://localhost:3333/api/randomQuote' });
+  t.true(quotes.body, 'Randowm Quote should be valid');
 });
 
-test("teardown", async (t) => {
+test('teardown', async (t) => {
   t.plan(1);
   await sandbox.end();
-  t.ok(true, "sandbox ended");
+  t.ok(true, 'sandbox ended');
 });
