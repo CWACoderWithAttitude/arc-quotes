dd81e060e51f989f400129ef465b0cf74b022ad4
Volker Benders <vbenders@gmail.com>
2023-04-29 22:29:01 +0000

    wip

--- src/http/get-api-quotes/index.mjs
+++ src/http/get-api-quotes/index.mjs
@@ -1,17 +1,17 @@
-import arc from '@architect/functions'
+import arc from '@architect/functions';
 
 // learn more about HTTP functions here: https://arc.codes/http
-export async function handler (req) {
+export async function handler(req) {
   //console.log("get-api-quotes: 1...")
-  let tables = await arc.tables()
-  const data = await tables.quotes.scan({})
-  console.log(">>> get-api-quotes: 2..." + data!=undefined ? data.Items:'n/a')
+  let tables = await arc.tables();
+  const data = await tables.quotes.scan({});
+  //console.log(">>> get-api-quotes: 2..." + data!=undefined ? data.Items:'n/a')
   return {
     statusCode: 200,
     headers: {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
-      'content-type': 'application/json; charset=utf8'
+      'content-type': 'application/json; charset=utf8',
     },
-    body: JSON.stringify(data.Items)
-  }
+    body: JSON.stringify(data.Items),
+  };
 }
--- tests/getQuotes-byLanguage-test.js
+++ tests/getQuotes-byLanguage-test.js
@@ -20,21 +20,31 @@ test('endpoint exists', async (t) => {
   let result = await tiny.get({ url: 'http://localhost:3333/api/quotes' });
   t.ok(result, 'got 200 response');
 });
-test('contenttype', async (t) => {
+test('get only german quotes', async (t) => {
   t.plan(3);
   let result = await tiny.get({ url: 'http://localhost:3333/api/quotes' });
-  t.ok(result.headers, 'no headers at all?!');
-  t.ok(result.headers['content-type'], 'no contewnrt tyoe header?!');
-  t.equal(result.headers['content-type'], 'application/json; charset=utf8', 'wrong content type');
+  t.ok(result, 'no headers at all?!');
+  const quotes = result.body;
+  const expectedLanguage = 'de';
+  const englishQuotes = quotes.filter((quote) => quote.language === expectedLanguage);
+  t.notEqual(englishQuotes, undefined, 'the result should contain quotes');
+  var wrongLanguageFound = false;
+  englishQuotes.forEach((quote) => {
+    if (quote.language !== expectedLanguage) {
+      wrongLanguageFound = true;
+    }
+  });
+  t.false(wrongLanguageFound, `only entries with language "${expectedLanguage}" should be in result`);
 });
-test('body', async (t) => {
+
+/* test('body', async (t) => {
   t.plan(3);
   let result = await tiny.get({ url: 'http://localhost:3333/api/quotes' });
   t.ok(result.body, 'no body in response?!');
   t.notEqual(result.body, 'itzelbritzel');
   t.equal(result.body.length, 73, 'wrong number of items in test data');
   //console.debug("test: getQuotes: quotes: " + JSON.stringify(result.body, " ", 2));
-});
+}); */
 
 test('teardown', async (t) => {
   t.plan(1);
